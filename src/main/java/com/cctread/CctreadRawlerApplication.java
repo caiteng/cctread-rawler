b1796cd3598bf7f6f2dbdbd9fc7470aba7c3693a
caiteng <caiteng@192.168.1.121>
2018-01-05 16:11:11 +0800

    Initial commit

---
+++ src/main/java/com/cctread/CctreadRawlerApplication.java
@@ -0,0 +1,12 @@
+package com.cctread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CctreadRawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CctreadRawlerApplication.class, args);
+	}
+}
