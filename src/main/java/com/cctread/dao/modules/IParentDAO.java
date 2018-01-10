cee98ed90f46cdf7c4c730bef407078d641cd7af
caiteng <caiteng@192.168.1.121>
2018-01-10 12:39:13 +0800

    补充
    Signed-off-by: caiteng <caiteng@192.168.1.121>

--- src/main/java/com/cctread/dao/modules/IParentDAO.java
+++ src/main/java/com/cctread/dao/modules/IParentDAO.java
@@ -9,24 +9,73 @@ import java.util.Map;
  *
  * @author cait
  * @version 1.0 创建时间：2018-01-08 16:21
+ * @deprecated 提供基本增删改查接口用以子接口继承
  */
 public interface IParentDAO<T, PK> {
 
+    /**
+     * 依据记录id查询记录
+     *
+     * @param id
+     * @return
+     */
     public T get(PK id);
 
+    /**
+     * 查询所有记录
+     *
+     * @return 结果集合
+     */
     public ArrayList<T> getAll();
 
+    /**
+     * 新增记录
+     *
+     * @param obj 数据库表对应的对象
+     */
     public void create(T obj);
 
+    /**
+     * 更新记录
+     *
+     * @param obj 数据库表对应的对象
+     */
     public void update(T obj);
 
+    /**
+     * 删除记录
+     *
+     * @param id 主键
+     */
     public void delete(PK id);
 
+    /**
+     * 批量删除
+     *
+     * @param parameters
+     */
     public void multiDelete(Map<String, ?> parameters);
 
+    /**
+     * 获取表总记录数
+     *
+     * @return
+     */
     public long count();
 
+    /**
+     * 依据条件查询相关记录
+     *
+     * @param parameters
+     * @return
+     */
     public List<T> findByWhere(Map<String, ?> parameters);
 
+    /**
+     * 依据条件查询相关记录总数
+     *
+     * @param parameters
+     * @return
+     */
     public Long getCountByWhere(Map<String, ?> parameters);
 }
