3347a1a773087b5dfde8b037d063110a4a398dd6
caiteng <caiteng@192.168.1.121>
2018-01-10 12:29:57 +0800

    补充
    Signed-off-by: caiteng <caiteng@192.168.1.121>

--- src/main/java/com/cctread/dao/RawlerTaskDao.java
+++ src/main/java/com/cctread/dao/RawlerTaskDao.java
@@ -4,6 +4,7 @@ package com.cctread.dao;
 
 import com.cctread.dao.modules.IParentDAO;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 /**
  * 爬虫任务
@@ -12,6 +13,7 @@ import org.apache.ibatis.annotations.Mapper;
  * @version 1.0 创建时间：2018-01-08 16:37
  */
 @Mapper
+@Repository
 public interface RawlerTaskDao extends IParentDAO {
 
 }
--- src/main/java/com/cctread/dao/modules/IParentDAO.java
+++ src/main/java/com/cctread/dao/modules/IParentDAO.java
@@ -1,7 +1,5 @@
 package com.cctread.dao.modules;
 
-import org.apache.ibatis.exceptions.PersistenceException;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
